0a3d04aadd293560700d0af6e52babb66ad290a7
Nikki Tiwari <nikkitiwari915@gmail.com>
2024-07-17 23:54:01 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+//<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from 'react';
+
+// Define the App component as a functional component
+const App = () => {
+  return (
+    <div id="main">
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  );
+}
+
+// Export the App component as the default export
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
+// Use ReactDOM.render to render the App component onto the DOM node with id="root"
+ReactDOM.render(<App />, document.getElementById('root'));
 
